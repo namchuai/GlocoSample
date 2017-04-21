22f5945b0e5b6f7f4d279392dc1f236a6420fcbd
Nam Nguyen <nam.nh1@altplus.com.vn>
2017-04-21 11:29:55 +0700

    add heroku server

--- app/src/main/java/com/vnindie/glocoapp/utils/AppConstant.java
+++ app/src/main/java/com/vnindie/glocoapp/utils/AppConstant.java
@@ -1,5 +1,5 @@
 package com.vnindie.glocoapp.utils;
 
 public final class AppConstant {
-  public static final String API_BASE_URL = "/";
+  public static final String API_BASE_URL = "https://serene-sea-84793.herokuapp.com/";
 }
